b110ed95d478d343dd85390ba6da2385919d0993
AndersRKvamme <rka092024@gmail.com>
2024-11-19 13:01:32 +0100

    Setup command matching

--- Program.cs
+++ Program.cs
@@ -7,12 +7,28 @@
 
         //Ta imot bruker kommando seleksjon
         Console.WriteLine ("Please enter your command.");
+        Console.WriteLine ("Available commands are as follows:");
+        Console.WriteLine ("-read");
+        Console.WriteLine ("-create");
         string? userInput = Console.ReadLine();
             if (userInput == null){
                 return; //Change later
             }
         // Finne Kommandoen
 
+
+        switch (userInput)
+        {
+            case "create":
+                Console.WriteLine("Creating file content...");
+                break;
+            case "read":
+                Console.WriteLine("Reading file content...");
+                break;
+            default:
+                Console.WriteLine("Unknown command.");
+                break;
+        }
         Console.WriteLine($"User inputted {userInput}");
 
 
