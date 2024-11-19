f208f4deb635d4e07fa905d14d951bf0f5e80b69
AndersRKvamme <rka092024@gmail.com>
2024-11-19 13:06:16 +0100

    Exit needs fix

--- Program.cs
+++ Program.cs
@@ -5,11 +5,16 @@
         //Skriv velckomst melding
         Console.WriteLine ("We welcome to C# File System Input/Output");
 
+        bool quitProgramme = false;
+            while (!quitProgramme)
+            {
+
         //Ta imot bruker kommando seleksjon
         Console.WriteLine ("Please enter your command.");
         Console.WriteLine ("Available commands are as follows:");
         Console.WriteLine ("-read");
         Console.WriteLine ("-create");
+        Console.WriteLine ("-quit");
         string? userInput = Console.ReadLine();
             if (userInput == null){
                 return; //Change later
@@ -25,12 +30,15 @@
             case "read":
                 Console.WriteLine("Reading file content...");
                 break;
+            case "quit":
+                Console.WriteLine("Exiting programme");
+                break;
             default:
                 Console.WriteLine("Unknown command.");
                 break;
         }
         Console.WriteLine($"User inputted {userInput}");
-
+}
 
 
     }
